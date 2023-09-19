1b6584b478dc37e23fc9a0481188e2a3e86bc35b
sobrown <zxrnhy1993@sina.com>
2023-09-19 16:17:32 +0800

    feat: add vuex

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ import "./plugins";
 Vue.config.productionTip = false;
 
 new Vue({
+    el: "#app",
     router,
     render: h => h(App),
 }).$mount("#app");
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,9 +1,12 @@
 <!--
  * @Author       : 张向荣
  * @Date         : 2023-09-06
- * @LastEditTime : 2023-09-06
+ * @LastEditTime : 2023-09-19
  * 
 -->
 <template>
-    <div>首页</div>
+    <div>
+        <div>首页</div>
+        <van-button>首页</van-button>
+    </div>
 </template>
